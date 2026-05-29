f6b51164225f4779f9cdf67d22adace795ae26ea
IMNOTPIZZA <48155178+bbeenkh@users.noreply.github.com>
2026-05-29 14:08:00 +0900

    chore: ios pod 관련 세팅

--- apps/mobile/DdasangfolioApp/metro.config.js
+++ apps/mobile/DdasangfolioApp/metro.config.js
@@ -1,11 +1,18 @@
+// apps/mobileApp/metro.config.js 
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
-
+/** 
+* Metro 설정 
+* <https://reactnative.dev/docs/metro> 
+* 
+* @type { import('metro-config').MetroConfig } 
+*/
+const config = {
+  resolver: {
+    unstable_enableSymlinks: true, // 심볼릭 링크 사용 활성화
+  },
+  // 프로젝트의 node_modules 폴더 위치 지정 
+  watchFolders: [path.join(__dirname, '..', '..')],
+};
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
