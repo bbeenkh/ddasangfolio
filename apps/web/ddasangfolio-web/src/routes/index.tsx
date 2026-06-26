446218bd6832d89d7a34e15c27de258509a09649
bbeenkh <boooong772@naver.com>
2026-06-26 15:02:32 +0900

    chore: 각 의존성 import 관련 작업 추가

--- apps/web/ddasangfolio-web/src/routes/index.tsx
+++ apps/web/ddasangfolio-web/src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@ddsf/ui';
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
     <main>
-      HELLO
+      <Button>버튼</Button>
     </main>
   )
 }
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./button";
+import { Card } from "./card";
+
+/**
+ * ddsf/ui 엔트리 영역
+ */
+export {
+  Button,
+  Card,
+}
